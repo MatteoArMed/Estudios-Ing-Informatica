d36d91cccb7811d4680eaec51f39a1d05ca5e1c1
Matteo Araneda <mate.araneda@duocuc.cl>
2022-08-24 16:40:43 -0400

    Actualizando Datos

---
+++ Act. 1 Sucesiones/Sucesiones.py
@@ -0,0 +1,20 @@
+# In VsCode download the extension "Fast Unicode Math Characters".
+
+# The hotkeys for ₂ is \_2.
+
+# The hotkeys for ² is \^2.
+
+# Sucesiones:
+#     -Una sucesion es una secuencia de terminos (Aₙ), donde cada uno de ellos esta asociado
+#       a un valor 'n', un numero natural que nos indica que lugar ocupa el termino.
+
+    # donde Aₙ representa el termino en el lugar n de la sucesion, con n=1, 2, 3, ...
+
+# Como n puede ser cualquier numero natural, eso quiere decir que una sucesion tiene infinitos terminos.
+
+# El nombre de la sucesion es una letra que usamos para identificarla, mientras que el subindice (ₓ)
+#     es una variable que etiqueta los elementos deu na sicesion, indicando el lugar que ocupan.
+
+# ¿Arreglos o listias?
+    # Lo Arreglos son estructuras ded atos estatiscas, ya que hay que declarar su tamaño antesde  utilizarlos.
+    # A diferencia de los Arreglos, las Listas son estructuras de datos dinamicas que pueden ir creciendo conforme se vaya requiriendo.
