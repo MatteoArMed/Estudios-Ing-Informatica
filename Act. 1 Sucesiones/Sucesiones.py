8b2dfc83e7c6a10e3ee08ce7cd66a51a2418ac8d
Matteo Araneda <mate.araneda@duocuc.cl>
2022-08-24 16:57:57 -0400

    Actualizando datos

--- Act. 1 Sucesiones/Sucesiones.py
+++ Act. 1 Sucesiones/Sucesiones.py
@@ -18,3 +18,35 @@
 # ¿Arreglos o listias?
     # Lo Arreglos son estructuras ded atos estatiscas, ya que hay que declarar su tamaño antesde  utilizarlos.
     # A diferencia de los Arreglos, las Listas son estructuras de datos dinamicas que pueden ir creciendo conforme se vaya requiriendo.
+
+# En este codigo, podemos ver como declarar una lista acotada y luego ir recorriendo sus elementos uno a uno
+
+b=[6,9,12,15,18] #Recordando que las listas de Python comienzan en 0, al indice contador se le suma 1 (+1)
+for i in range(len(b)): #Con len(), contamos la lista y vamos agregando +1 al indice.
+    n=i+1
+    print(f'b({n})={b[i]}')
+
+# Podemos generar los cuatro primeros terminos de esta sucesion usando el sig. codigo:
+a=[]
+for i in range(10):
+    n=i+1
+    a.append(3*n)
+    print(f'a({n})={a[i]}')
+#Sabiendo cual es la formula podemos agregarla a la lista e iterarla para mostrar los terminos que necesitemos.
+
+#  Se define la sucesion (Bₙ) como 
+#     Bₙ = 5*0.92ₙ
+# donde queremos calcular y mostrar en pantalla desde B₁₀ hasta B₁₅. El problema es que si bien es cierto se pueden
+# agregar elemenos, pero necesariamente deben existir los 9 elementos anteriores a B₁₀, de lo contrario Python arrojara error. 
+
+# Entonces para no calcular elementos innecesarios usaremos la expresion
+#     N = i + 10 Siendo '10' el numero del termino donde queremos empezar.
+# con la que haremos que la lista asociada a esta sucesion inicie con B[0] y al mismo tiempo comenzamos calculando 
+# B₁₀.
+
+b=[]
+#al usar rangue(6) la variable "i" asume valores enteross desde 0 hasta 5
+for i in range(6): #Se agrega un termino mas para que nos tome hasta el anterior a este.
+    n=i+10 #Se le suma +10 ya que es el termino donde vamos a comenzar
+    b.append(5*0.92**n) #Se agrega la secuencia en la lista para poder guardarla e iterarla.
+    print(f'b({n})={b[i]}')
